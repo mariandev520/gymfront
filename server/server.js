24dca144694b6a560ee7744d58647be1b9403931
mariandev520 <manzomariano@hotmail.com>
2025-03-09 00:09:08 -0300

    base inflexion 9

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ mongoose
 // Configurar CORS
 const allowedOrigins = [
   "http://localhost:3002",
+  "https://gymfront.vercel.app/",
   "http://gymfront-git-conmogose-mariandev520s-projects.vercel.app",
   "http://192.168.1.41:3002",
   "https://06fc-201-178-206-232.ngrok-free.app", // Agregar la URL de ngrok
