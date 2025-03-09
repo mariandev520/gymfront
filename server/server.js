355c69048b1ac4b273e44de47b73981163f42b08
mariandev520 <manzomariano@hotmail.com>
2025-03-09 00:54:50 -0300

    base inflexion 12

--- server/server.js
+++ server/server.js
@@ -11,26 +11,25 @@ const profesoresRoutes = require("./routes/profesores");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({ origin: '*' }));
-
 // 🔹 Conectar a MongoDB
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/basediego";
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("🔥 Conectado a MongoDB"))
   .catch((error) => {
     console.error("❌ Error al conectar a MongoDB:", error);
     process.exit(1);
   });
 
-// Configurar CORS
+// 🔹 Configurar CORS (Permitir solo ciertos dominios)
 const allowedOrigins = [
   "http://localhost:3002",
-  "https://gymfront.vercel.app/",
+  "https://gymfront.vercel.app",
   "http://gymfront-git-conmogose-mariandev520s-projects.vercel.app",
   "http://192.168.1.41:3002",
-  "https://42f6-201-178-206-232.ngrok-free.app/", // Agregar la URL de ngrok
+  "https://06fc-201-178-206-232.ngrok-free.app" // URL de ngrok si la usas
 ];
+
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -40,15 +39,15 @@ app.use(
         callback(new Error("Origen no permitido por CORS"));
       }
     },
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// Middleware
+// Middleware para interpretar JSON
 app.use(express.json());
 
-// Rutas
+// 🔹 Rutas de la API
 app.use("/clientes", clientesRoutes);
 app.use("/actividades", actividadesRoutes);
 app.use("/profesores", profesoresRoutes);
@@ -58,18 +57,18 @@ app.get("/", (req, res) => {
   res.send("🔥 API funcionando correctamente!");
 });
 
-// Manejo de errores global
+// 🔹 Manejo de errores global
 app.use((err, req, res, next) => {
   console.error("❌ Error:", err.message);
   res.status(500).json({ message: "Error interno del servidor" });
 });
 
-// Ruta no encontrada
+// 🔹 Ruta no encontrada
 app.use((req, res) => {
   res.status(404).json({ message: "Ruta no encontrada" });
 });
 
-// Iniciar el servidor
+// 🔹 Iniciar el servidor en 0.0.0.0 para permitir accesos externos
 app.listen(port, "0.0.0.0", () => {
-  console.log(`✅ Servidor ejecutándose en http://localhost:${port}`);
+  console.log(`✅ Servidor ejecutándose en http://192.168.1.41:${port}`);
 });
--- src/components/admin/Clientes.js
+++ src/components/admin/Clientes.js
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_BASE_URL = "https://5ac6-2802-8012-2930-a901-6197-9b85-2698-663a.ngrok-free.app";
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { "ngrok-skip-browser-warning": "true" },
+});
 
 const Clientes = () => {
   const [clientes, setClientes] = useState([]);
@@ -7,35 +14,38 @@ const Clientes = () => {
   const [profesores, setProfesores] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newCliente, setNewCliente] = useState({
-    nombre: '',
-    direccion: '',
-    correo: '',
-    telefono: '',
-    tarifa_mensual: '',
+    nombre: "",
+    direccion: "",
+    correo: "",
+    telefono: "",
+    tarifa_mensual: "",
     actividades: [],
     profesores: [],
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [selectedActividad, setSelectedActividad] = useState(null);
 
   // Obtener actividades y profesores al cargar el componente
   useEffect(() => {
-    axios.get('https://42f6-201-178-206-232.ngrok-free.app/actividades')
-      .then(response => setActividades(response.data))
-      .catch(error => console.error('Error fetching actividades:', error));
+    axiosInstance
+      .get("/actividades")
+      .then((response) => setActividades(response.data))
+      .catch((error) => console.error("Error fetching actividades:", error));
 
-    axios.get('https://42f6-201-178-206-232.ngrok-free.app/profesores')
-      .then(response => setProfesores(response.data))
-      .catch(error => console.error('Error fetching profesores:', error));
+    axiosInstance
+      .get("/profesores")
+      .then((response) => setProfesores(response.data))
+      .catch((error) => console.error("Error fetching profesores:", error));
 
     fetchClientes();
   }, []);
 
   // Obtener la lista de clientes
   const fetchClientes = () => {
-    axios.get('https://42f6-201-178-206-232.ngrok-free.app/clientes/clientes-con-profesores-y-actividades')
-      .then(response => setClientes(response.data))
-      .catch(error => console.error('Error fetching clientes:', error));
+    axiosInstance
+      .get("/clientes/clientes-con-profesores-y-actividades")
+      .then((response) => setClientes(response.data))
+      .catch((error) => console.error("Error fetching clientes:", error));
   };
 
   // Manejar cambios en los inputs del formulario
@@ -48,31 +58,32 @@ const Clientes = () => {
   const handleSelectChange = (e) => {
     const { name, options } = e.target;
     const selectedValues = Array.from(options)
-      .filter(option => option.selected)
-      .map(option => option.value);
+      .filter((option) => option.selected)
+      .map((option) => option.value);
     setNewCliente({ ...newCliente, [name]: selectedValues });
   };
 
   // Enviar el nuevo cliente al backend
   const handleSubmitNewCliente = (e) => {
     e.preventDefault();
-    axios.post('https://42f6-201-178-206-232.ngrok-free.app/clientes', newCliente)
-      .then(response => {
+    axiosInstance
+      .post("/clientes", newCliente)
+      .then((response) => {
         setClientes([...clientes, response.data]);
         setNewCliente({
-          nombre: '',
-          direccion: '',
-          correo: '',
-          telefono: '',
-          tarifa_mensual: '',
+          nombre: "",
+          direccion: "",
+          correo: "",
+          telefono: "",
+          tarifa_mensual: "",
           actividades: [],
           profesores: [],
         });
         setIsModalOpen(false);
       })
-      .catch(error => {
-        console.error('Error al agregar el cliente:', error);
-        setError('Hubo un problema al agregar el cliente.');
+      .catch((error) => {
+        console.error("Error al agregar el cliente:", error);
+        setError("Hubo un problema al agregar el cliente.");
       });
   };
 
@@ -82,9 +93,12 @@ const Clientes = () => {
       fetchClientes();
       setSelectedActividad(null);
     } else {
-      axios.get(`https://42f6-201-178-206-232.ngrok-free.app/filtrar-por-actividad/${actividad}`)
-        .then(response => setClientes(response.data))
-        .catch(error => console.error('Error filtrando clientes por actividad:', error));
+      axiosInstance
+        .get(`/clientes/filtrar-por-actividad/${actividad}`)
+        .then((response) => setClientes(response.data))
+        .catch((error) =>
+          console.error("Error filtrando clientes por actividad:", error)
+        );
       setSelectedActividad(actividad);
     }
   };
@@ -94,7 +108,9 @@ const Clientes = () => {
 
   return (
     <div className="container bg-black p-4">
-      <h1 className="text-4xl font-bold text-center mb-8 text-white">Clientes</h1>
+      <h1 className="text-4xl font-bold text-center mb-8 text-white">
+        Clientes
+      </h1>
 
       {/* Botón para abrir el modal de agregar cliente */}
       <button
@@ -112,8 +128,8 @@ const Clientes = () => {
             onClick={() => handleFilterByActividad(actividad)}
             className={`cursor-pointer px-4 py-2 rounded transition duration-300 mr-4 mb-2 inline-block ${
               selectedActividad === actividad
-                ? 'bg-blue-200 text-white'
-                : 'hover:bg-green'
+                ? "bg-blue-200 text-white"
+                : "hover:bg-green"
             }`}
           >
             {actividad}
@@ -121,139 +137,14 @@ const Clientes = () => {
         ))}
       </div>
 
-      {/* Modal para agregar cliente */}
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-2xl">
-            <h2 className="text-2xl font-semibold mb-4">Agregar Cliente</h2>
-            <form onSubmit={handleSubmitNewCliente}>
-              <div className="mb-4">
-                <label className="block text-gray-700">Nombre</label>
-                <input
-                  type="text"
-                  name="nombre"
-                  value={newCliente.nombre}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  placeholder="Nombre del cliente"
-                  required
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Dirección</label>
-                <input
-                  type="text"
-                  name="direccion"
-                  value={newCliente.direccion}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  placeholder="Dirección del cliente"
-                  required
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Correo</label>
-                <input
-                  type="email"
-                  name="correo"
-                  value={newCliente.correo}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  placeholder="Correo del cliente"
-                  required
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Teléfono</label>
-                <input
-                  type="text"
-                  name="telefono"
-                  value={newCliente.telefono}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  placeholder="Teléfono del cliente"
-                  required
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Tarifa Mensual</label>
-                <input
-                  type="number"
-                  name="tarifa_mensual"
-                  value={newCliente.tarifa_mensual}
-                  onChange={handleInputChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  placeholder="Tarifa mensual"
-                  required
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Actividades</label>
-                <select
-                  name="actividades"
-                  value={newCliente.actividades}
-                  onChange={handleSelectChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  multiple
-                  required
-                >
-                  {actividades.map(actividad => (
-                    <option key={actividad._id} value={actividad._id}>
-                      {actividad.nombre}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Profesores</label>
-                <select
-                  name="profesores"
-                  value={newCliente.profesores}
-                  onChange={handleSelectChange}
-                  className="w-full p-2 border border-gray-300 rounded mt-1"
-                  multiple
-                  required
-                >
-                  {profesores.map(profesor => (
-                    <option key={profesor._id} value={profesor._id}>
-                      {profesor.nombre}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              <div className="flex justify-end space-x-4">
-                <button
-                  type="button"
-                  onClick={() => setIsModalOpen(false)}
-                  className="bg-gray-500 text-white px-8 py-4 rounded hover:bg-gray-500 transition duration-300"
-                >
-                  Cancelar
-                </button>
-                <button
-                  type="submit"
-                  className="bg-green-400 text-white px-4 py-2 rounded hover:bg-black transition duration-300"
-                >
-                  Agregar Cliente
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-
       {/* Mostrar errores */}
       {error && <div className="p-4 bg-red-500 text-white">{error}</div>}
 
       {/* Tabla de clientes */}
       <section>
-        <h2 className="text-2xl font-semibold mb-4 text-white">Lista de Clientes</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-white">
+          Lista de Clientes
+        </h2>
         <div className="bg-gray-900 p-6 rounded-lg shadow-md overflow-x-auto">
           <table className="w-full table-auto">
             <thead>
@@ -263,44 +154,42 @@ const Clientes = () => {
                 <th className="hidden sm:table-cell px-4 py-2">Dirección</th>
                 <th className="hidden sm:table-cell px-4 py-2">Teléfono</th>
                 <th className="hidden sm:table-cell px-4 py-2">Correo</th>
-                <th className="hidden sm:table-cell px-4 py-2">Tarifa Mensual</th>
+                <th className="hidden sm:table-cell px-4 py-2">
+                  Tarifa Mensual
+                </th>
               </tr>
             </thead>
             <tbody>
               {clientes.map((cliente) => {
                 // Convertir IDs de actividades a nombres
-                const actividadesNombres = cliente.actividades.map(id => {
-                  const actividad = actividades.find(a => a._id === id);
-                  return actividad ? actividad.nombre : "Desconocido";
-                }).join(', ');
-
-                // Convertir IDs de profesores a nombres
-                const profesoresNombres = cliente.profesores.map(id => {
-                  const profesor = profesores.find(p => p._id === id);
-                  return profesor ? profesor.nombre : "Desconocido";
-                }).join(', ');
+                const actividadesNombres = cliente.actividades
+                  .map((id) => {
+                    const actividad = actividades.find((a) => a._id === id);
+                    return actividad ? actividad.nombre : "Desconocido";
+                  })
+                  .join(", ");
 
                 return (
                   <React.Fragment key={cliente._id}>
                     {/* Fila principal (Nombre y Actividad) */}
                     <tr className="border-4 border-gray-700 rounded hover:bg-gray-900 text-white transition duration-300">
-                      <td className="px-4 bg-gray-900 text-gray-300 py-2">{cliente.nombre}</td>
-                      <td className="px-4 bg-gray-900 text-gray-300 py-2">{actividadesNombres}</td>
-                      {/* Columnas ocultas en móviles */}
-                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">{cliente.direccion}</td>
-                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">{cliente.telefono}</td>
-                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">{cliente.correo}</td>
-                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">{cliente.tarifa_mensual}</td>
-                    </tr>
-                    {/* Fila adicional para móviles (detalles) */}
-                    <tr className="sm:hidden">
-                      <td colSpan="2" className="px-4 bg-gray-900 text-gray-300 py-2">
-                        <div className="flex flex-col space-y-2">
-                          <p><span className="font-bold">Dirección:</span> {cliente.direccion}</p>
-                          <p><span className="font-bold">Teléfono:</span> {cliente.telefono}</p>
-                          <p><span className="font-bold">Correo:</span> {cliente.correo}</p>
-                          <p><span className="font-bold">Tarifa Mensual:</span> {cliente.tarifa_mensual}</p>
-                        </div>
+                      <td className="px-4 bg-gray-900 text-gray-300 py-2">
+                        {cliente.nombre}
+                      </td>
+                      <td className="px-4 bg-gray-900 text-gray-300 py-2">
+                        {actividadesNombres}
+                      </td>
+                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">
+                        {cliente.direccion}
+                      </td>
+                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">
+                        {cliente.telefono}
+                      </td>
+                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">
+                        {cliente.correo}
+                      </td>
+                      <td className="hidden sm:table-cell px-4 bg-gray-900 text-gray-300 py-2">
+                        {cliente.tarifa_mensual}
                       </td>
                     </tr>
                   </React.Fragment>
